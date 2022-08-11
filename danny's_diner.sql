c49dfe8e897344b1198aeead7da9f7fcba129679
davidrtorres041 <54711338+davidrtorres@users.noreply.github.com>
2022-08-11 17:53:47 -0400

    update

--- danny's_diner.sql
+++ danny's_diner.sql
@@ -1,5 +1,5 @@
 -- Danny's Diner Case Study Questions
--- 8/9/22
+
 -- What is the total amount each customer spent at the restaurant?
 -- Customer A spent 76.00, Customer B spent 74.00 Customer c spent 36.00.
 SELECT * FROM dannys_diner.sales;
@@ -16,4 +16,12 @@ SELECT
 FROM dannys_diner.sales
 INNER JOIN dannys_diner.menu
 ON sales.product_id = menu.product_id
+GROUP BY customer_id;
+
+-- 2.How many days has each customer visited the restaurant?
+-- A visted 4 days, B visited 6 days, C visited 2 days
+SELECT
+	customer_id,
+    COUNT(DISTINCT order_Date) AS days_visited
+FROM dannys_diner.sales
 GROUP BY customer_id;
