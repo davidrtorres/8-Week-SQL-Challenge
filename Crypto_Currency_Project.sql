05edf7f208369cba65bec77c097b71b564e3a985
davidrtorres041 <54711338+davidrtorres@users.noreply.github.com>
2022-08-14 16:06:13 -0400

    updates

---
+++ Crypto_Currency_Project.sql
@@ -0,0 +1,47 @@
+-- Sort all the rows in the table by first_name in alphabetical order and show the top 3 rows
+SELECT * FROM trading.members ORDER BY first_name LIMIT 3;
+
+-- Which records from trading.members are from the United States region?
+SELECT * FROM trading.members WHERE region = 'United States';
+
+-- Select only the member_id and first_name columns for members who are not from Australia
+SELECT 
+	member_id,	
+	first_name 
+FROM trading.members 
+WHERE region != 'Australia';
+
+-- Return the unique region values from the trading.members table and sort the output by reverse alphabetical order
+SELECT
+	DISTINCT region AS distinct_regions
+FROM trading.members
+ORDER BY distinct_regions DESC;
+
+-- How many memtors are there from Australia or the United States?
+-- There are 7 members from U.S. and 4 members from Austrlia
+SELECT
+	region,
+    COUNT(*)
+FROM trading.members
+WHERE region = 'United States' OR region='Australia'
+GROUP BY region;
+
+-- How many mentors are not from Australia or the United States?
+-- 3 mentors aren't from US or Australia.
+SELECT
+	COUNT(*)
+FROM trading.members
+WHERE region NOT IN ('United States','Australia');
+
+-- How many mentors are there per region? Sort the output by regions with the most mentors to the least?
+SELECT
+	region,
+    COUNT(*) AS member_count 
+FROM trading.members
+GROUP BY region 
+ORDER BY member_count;
+
+
+
+
+
